614c6c7dc506927c29f56761e8691da1c69d6052
osnory <offer@nory.ai>
2023-03-09 11:23:47 +0200

    try this

--- musika/settings.py
+++ musika/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'musika.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("MUSIKA_DB_NAME", 'postgres'),
+        'NAME': os.environ.get("MUSIKA_DB_NAME", 'musika'),
         'USER': os.environ.get("MUSIKA_DB_USER", 'postgres'),
         'PASSWORD': os.environ.get("MUSIKA_DB_PASSWORD", 'postgres'),
-        'HOST': os.environ.get("MUSIKA_DB_HOST", 'postgres'),
+        'HOST': os.environ.get("MUSIKA_DB_HOST", 'db'),
         'PORT': os.environ.get("MUSIKA_DB_PORT", '5432'),
     }
 }

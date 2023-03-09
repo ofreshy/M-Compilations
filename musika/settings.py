2b275526e72a3b7b16fcf05e64f15c6fdb14c7fb
osnory <offer@nory.ai>
2023-03-09 11:56:26 +0200

    try this

--- musika/settings.py
+++ musika/settings.py
@@ -85,17 +85,6 @@ DATABASES = {
     }
 }
 
-# if os.environ.get('GITHUB_WORKFLOW'):
-#     DATABASES = {
-#         'default': {
-#            'ENGINE': 'django.db.backends.postgresql',
-#            'NAME': 'musika',
-#            'USER': 'postgres',
-#            'PASSWORD': 'postgres',
-#            'HOST': '127.0.0.1',
-#            'PORT': '5432',
-#         }
-#     }
 
 
 # Password validation
